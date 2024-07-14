8a95787ab6a63cf258d8745f4be60dfcd7213e32
mehdidotcafe <contact@mehdi.cafe>
2024-07-14 21:36:08 +0200

    add gitignore

--- node_modules/peek-readable/lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { EndOfStreamError } from './EndOfStreamError.js';
-export { StreamReader } from './StreamReader.js';
-export { WebStreamReader } from './WebStreamReader.js';
-export { IStreamReader } from './AbstractStreamReader.js';
